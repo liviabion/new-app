6c526f3696a5500e49eab0f30563ae92576996cc
liviabion <liviabion@gmail.com>
2024-12-02 15:55:25 -0300

    feat: calculator functions

--- app/src/main/java/bion/lima/livia/myfirstproject/CalculatorActivity.kt
+++ app/src/main/java/bion/lima/livia/myfirstproject/CalculatorActivity.kt
@@ -5,12 +5,45 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import bion.lima.livia.myfirstproject.databinding.ActivityCalculatorBinding
+import bion.lima.livia.myfirstproject.databinding.ActivityMainBinding
 
 class CalculatorActivity : ComponentActivity() {
 
+    private lateinit var binding: ActivityCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calculator)
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.soma.setOnClickListener() {
+            var value1 = binding.primeiroNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var value2 = binding.segundoNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var soma:Double = value1 + value2
+            binding.resultado.text = soma.toString()
+        }
+
+        binding.multiplicacao.setOnClickListener() {
+            var value1 = binding.primeiroNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var value2 = binding.segundoNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var mult:Double = value1 * value2
+            binding.resultado.text = mult.toString()
+        }
+
+        binding.subtracao.setOnClickListener() {
+            var value1 = binding.primeiroNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var value2 = binding.segundoNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var sub:Double = value1 - value2
+            binding.resultado.text = sub.toString()
+        }
+
+        binding.divisao.setOnClickListener() {
+            var value1 = binding.primeiroNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var value2 = binding.segundoNumero.text.toString().toDoubleOrNull() ?: 0.0
+            var div:Double = value1 / value2
+            binding.resultado.text = div.toString()
+        }
 
     }
 }
