c354cf87c23b03d7e2d9221b655e9d88c443159a
liviabion <liviabion@gmail.com>
2024-12-02 15:11:05 -0300

    feat: calculator structure

---
+++ app/src/main/java/bion/lima/livia/myfirstproject/CalculatorActivity.kt
@@ -0,0 +1,16 @@
+package bion.lima.livia.myfirstproject;
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class CalculatorActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+    }
+}
--- app/src/main/java/bion/lima/livia/myfirstproject/MainActivity.kt
+++ app/src/main/java/bion/lima/livia/myfirstproject/MainActivity.kt
@@ -1,20 +1,10 @@
 package bion.lima.livia.myfirstproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import bion.lima.livia.myfirstproject.databinding.ActivityMainBinding
-import bion.lima.livia.myfirstproject.ui.theme.MyFirstProjectTheme
+
 
 class MainActivity : ComponentActivity() {
 
@@ -28,12 +18,22 @@ class MainActivity : ComponentActivity() {
 //        val textView = findViewById<TextView>(R.id.textView)
 //        val changeButton: Button = findViewById(R.id.changeButton)
 
-        binding.unchangeButton.setOnClickListener(){
+        binding.changeButton.setOnClickListener() {
             binding.textView.text = "Oiii"
         }
 
 //        changeButton.setOnClickListener(){
 //            textView.text = "Oi, Lívia!"
 //        }
+
+
+        binding.calculatorButton.setOnClickListener { v ->
+            val intent =
+                Intent(
+                    this@MainActivity,
+                    CalculatorActivity::class.java
+                )
+            startActivity(intent)
+        }
     }
 }
